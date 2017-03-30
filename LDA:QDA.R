3ca369717f71d267af8a0d0b5c3fea8ac673b80d
elmerleeZ <zongyangli@172-16-208-197.DYNAPOOL.NYU.EDU>
2017-03-29 23:15:41 -0400

    LDA/QDA: start example

---
+++ LDA:QDA.R
@@ -0,0 +1,4 @@
+setwd('/Users/zongyangli/Google Drive/Wagner/Semester 4/Capstone/Capstone 2016-2017/Literature /Method/QDA')
+
+beetles <- read.table('T5_5_FBEETLES.DAT')
+, col.names = c('Measurement.Number', 'Species', 'transverse.groove.dist', 'elytra.length', 'second.antennal.joint.length', 'third.antennal.joint.length'))
